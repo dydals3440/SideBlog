e687e0b501573d09e3b55f69768b81655ae7d635
김용민 <dydals3440@gmail.com>
2023-10-11 19:17:14 +0900

    feat: server route nodemailer

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,3 +1,4 @@
+import { sendEmail } from '@/service/email';
 import * as yup from 'yup';
 
 const bodySchema = yup.object().shape({
@@ -7,9 +8,29 @@ const bodySchema = yup.object().shape({
 });
 
 export async function POST(req: Request) {
-  if (!bodySchema.isValidSync(req.body)) {
-    return new Response('유효하지 않은, 이메일 양식입니다', { status: 400 });
+  // redableStream을 전환해줘야함.
+  const body = await req.json();
+  console.log(body);
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(
+      JSON.stringify({ message: '메일 전송에 실패했습니다!' }),
+      { status: 400 }
+    );
   }
-  const { from, subject, message } = req.body;
-  // Send Mail with nodemailer
+
+  return sendEmail(body)
+    .then(
+      () =>
+        new Response(
+          JSON.stringify({ message: '메일을 성공적으로 전송했습니다.' }),
+          { status: 200 }
+        )
+    )
+    .catch((error) => {
+      console.error(error);
+      return new Response(
+        JSON.stringify({ message: '메일 전송에 실패했습니다!' }),
+        { status: 200 }
+      );
+    });
 }
